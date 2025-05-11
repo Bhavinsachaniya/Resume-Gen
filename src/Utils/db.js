fe4e7b7604d7993b419a19936ac6f380381567cb
Bhavin Sachaniya <bhavinbhavin494@gmail.com>
2025-05-12 01:47:18 +0530

    Updated db.js to use MONGO_URI environment variable for MongoDB connection

--- src/Utils/db.js
+++ src/Utils/db.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-        const conn = await mongoose.connect(process.env.mongo_db)
-            .then(() => console.log('MongoDB connected'))
-            .catch(err => console.log(err)); 
-};
+// Fetch MongoDB URI from environment variable
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+  console.error("MongoDB URI not found in environment variables!");
+  process.exit(1); // Exit the process if no URI is provided
+}
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error: ", err));
 
 module.exports = connectDB;
